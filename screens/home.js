8ce451e083f459c5dfd24c957f93add2b4cad025
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-07 21:43:28 +0200

    Add personal info

--- screens/home.js
+++ screens/home.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   Image,
+  ScrollView,
 } from 'react-native';
 import { MAIN_COLOR, SECONDARY_COLOR } from '../constants';
 
@@ -21,7 +22,7 @@ const Home = () => {
             source = {require('../assets/profile.jpg')}
             style = {{
               width: 140, height: 140, borderRadius: 70,
-              borderColor: SECONDARY_COLOR, borderWidth: 2
+              borderColor: SECONDARY_COLOR, borderWidth: 0.5
             }}
           />
           <View style = {{
@@ -32,18 +33,47 @@ const Home = () => {
               color: 'white', fontSize: 25
             }}>Marwan Rashed</Text>
             <Text style = {{
-              color: SECONDARY_COLOR, fontSize: 15,
+              color: SECONDARY_COLOR, fontSize: 20,
               marginTop: 10
             }}>Entry-Level Software Engineer</Text>
           </View>
         </View>
-        <View style = {{
-          width: '90%', height: '75%',
-          backgroundColor: 'white',
-          borderTopRightRadius: 25, borderTopLeftRadius: 25
-        }}>
-            
-        </View>
+        <ScrollView
+          contentContainerStyle = {{
+            alignItems: 'center'
+          }}
+          style = {{
+            width: '90%', height: '75%',
+            backgroundColor: 'white',
+            borderTopRightRadius: 25, borderTopLeftRadius: 25,
+            padding: 20,
+          }}
+        >
+          <View style = {{
+            width: '100%', padding: 10, marginBottom: 20,
+            borderWidth: 1, borderColor: SECONDARY_COLOR, borderRadius: 10
+          }}>
+              <Text style = {{
+                color: SECONDARY_COLOR, marginBottom: 5
+              }}>Full Name :</Text>
+              <Text>Marwan Mohamed Mohamed Rashed</Text>
+              <Text style = {{
+                color: SECONDARY_COLOR, marginBottom: 5, marginTop: 5
+              }}>Email :</Text>
+              <Text>marwanrashed300@gmail.com</Text>
+              <Text style = {{
+                color: SECONDARY_COLOR, marginBottom: 5, marginTop: 5
+              }}>Mobile :</Text>
+              <Text>+20 115 410 3808</Text>
+              <Text>+20 114 380 4555</Text>
+          </View>
+          <View style = {{
+            width: '100%', padding: 10, marginBottom: 20,
+            borderWidth: 1, borderColor: SECONDARY_COLOR, borderRadius: 10
+          }}>
+
+          </View>
+        </ScrollView>
     </View>
   );
 }
